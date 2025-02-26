0a9d87a2e5c659387f521290feab8721e3c40a5b
Zephyr-Jeon <rltjd1258@gmail.com>
2025-02-25 19:49:01 -0800

    added AccountRepo, AuthService and Validator to di

--- src/di/di.ts
+++ src/di/di.ts
@@ -1,7 +1,10 @@
 import { IAppConfigs } from '../configs/config.interface';
 import { appConfigs } from '../configs/getAppConfigs';
 import { DB } from '../db/db';
-import { Container } from './Container';
+import { AccountRepository } from '../repositories/AccountRepository';
+import { AuthService } from '../services/AuthService';
+import { validator, Validator } from '../validator/Validator';
+import { Container, SCOPE } from './Container';
 
 export class DI {
   private container = new Container<DI_KEY>();
@@ -21,12 +24,29 @@ export class DI {
   }
 
   registerDeps() {
+    this.container.register(DI_KEY.DI, { value: di });
     this.container.register(DI_KEY.CONFIGS, { value: appConfigs });
+    this.container.register(DI_KEY.VALIDATOR, { value: validator });
     this.container.register(DI_KEY.DB, {
       constructor: DB,
       deps: [DI_KEY.CONFIGS],
-      scope: 'singleton',
+      scope: SCOPE.SINGLETON,
     });
+    this.container.register(DI_KEY.ACCOUNT_REPOSITORY, {
+      constructor: AccountRepository,
+      deps: [DI_KEY.DB],
+      scope: SCOPE.SINGLETON,
+    });
+    this.container.register(DI_KEY.AUTH_SERVICE, {
+      constructor: AuthService,
+      deps: [DI_KEY.DI, DI_KEY.ACCOUNT_REPOSITORY],
+      scope: SCOPE.SINGLETON,
+    });
+
+    // initialize
+    this.db;
+    this.authRepository;
+    this.authService;
 
     return this;
   }
@@ -35,14 +55,30 @@ export class DI {
     return this.resolve<IAppConfigs>(DI_KEY.CONFIGS);
   }
 
+  get validator() {
+    return this.resolve<Validator>(DI_KEY.VALIDATOR);
+  }
+
   get db() {
     return this.resolve<DB>(DI_KEY.DB);
+  }
+
+  get authRepository() {
+    return this.resolve<DI_KEY.ACCOUNT_REPOSITORY>(DI_KEY.ACCOUNT_REPOSITORY);
+  }
+
+  get authService() {
+    return this.resolve<AuthService>(DI_KEY.AUTH_SERVICE);
   }
 }
 
 export enum DI_KEY {
+  DI = 'DI',
   CONFIGS = 'CONFIGS',
+  VALIDATOR = 'VALIDATOR',
   DB = 'DB',
+  ACCOUNT_REPOSITORY = 'ACCOUNT_REPOSITORY',
+  AUTH_SERVICE = 'AUTH_SERVICE',
 }
 
 export const di = DI.getInstance();
